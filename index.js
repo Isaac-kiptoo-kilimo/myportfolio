6f8707a80035976f330769fb0704b3578c1dda46
Isaac-kiptoo-kilimo <isaac.kilimo@student.moringaschool.com>
2022-02-26 07:35:00 +0300

    style:index.js-this will display hidden description on project section

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-// const all_projects = document.getElementsByClassName('project');
+const all_projects = document.getElementsByClassName('project');
 
 
-// for (let i = 0; i < all_projects.length; i++) {
-//   const proj = all_projects[i];
-//   proj.addEventListener('mouseenter', function () {
-//     const proj_img_holder = proj.getElementsByClassName('project-img')[0];
-//     const img_ = proj_img_holder.getElementsByTagName('img')[0]
-//     const img_offset = img_.offsetHeight;
-//     proj_img_holder.scroll({ behavior: 'smooth', block: 'center', inline: 'center', top: img_offset });
-//   });
-// }
+for (let i = 0; i < all_projects.length; i++) {
+  const proj = all_projects[i];
+  proj.addEventListener('mouseenter', function () {
+    const proj_img_holder = proj.getElementsByClassName('project-img')[0];
+    const img_ = proj_img_holder.getElementsByTagName('img')[0]
+    const img_offset = img_.offsetHeight;
+    proj_img_holder.scroll({ behavior: 'smooth', block: 'center', inline: 'center', top: img_offset });
+  });
+}
 
-// for (let i = 0; i < all_projects.length; i++) {
-//   const proj = all_projects[i];
-//   proj.addEventListener('mouseleave', function () {
-//     const proj_img_holder = proj.getElementsByClassName('project-img')[0];
-//     // const img_ = proj_img_holder.getElementsByTagName('img')[0]
-//     // const img_offset = img_.offsetHeight;
-//     proj_img_holder.scroll({ behavior: 'smooth', block: 'end', inline: 'center', top: 0 });
-//   });
-// }
+for (let i = 0; i < all_projects.length; i++) {
+  const proj = all_projects[i];
+  proj.addEventListener('mouseleave', function () {
+    const proj_img_holder = proj.getElementsByClassName('project-img')[0];
+    // const img_ = proj_img_holder.getElementsByTagName('img')[0]
+    // const img_offset = img_.offsetHeight;
+    proj_img_holder.scroll({ behavior: 'smooth', block: 'end', inline: 'center', top: 0 });
+  });
+}
